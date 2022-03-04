022092ea5bae5686cea7289b6a35887f5c46bad9
Quang Minh <54538472+quangmnh@users.noreply.github.com>
2022-03-04 17:36:24 +0700

    bruh

--- camera_test.py
+++ camera_test.py
@@ -3,7 +3,7 @@ capture = cv2.VideoCapture(0)
 
 while True:
     _, frame = capture.read()
-    cv2.imshow('Emotion :3', frame)
+    cv2.imshow('Emotion :3', cv2.resize(frame, (150,100)))
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
